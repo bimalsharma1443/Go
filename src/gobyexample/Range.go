274d774c9f13777a911907ba12117213f61108f5
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-03-04 22:46:21 +0530

    range

---
+++ src/gobyexample/Range.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	num := []int{1,2,3,4}
+
+	sum := 0
+	for _,v := range num  {
+		sum += v
+	}
+
+	fmt.Println(sum)
+
+	kvs := map[string]string{"a":"apple","b":"mango"}
+	for k,v := range kvs {
+		fmt.Printf("%s -> %s\n",k,v)
+	}
+	
+	for k := range kvs {
+		fmt.Println("key : ",k)
+	}
+
+	for k,v := range "golang" {
+		fmt.Println(k,v)
+	}
+
+
+}
