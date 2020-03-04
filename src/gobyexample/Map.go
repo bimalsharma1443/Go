ea6f2a3ef288c40ebe11f849134f46f0b5e57a69
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-03-04 22:26:31 +0530

    map

---
+++ src/gobyexample/Map.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// create map 
+	// map[key-type]value-type
+	m := make(map[string]int)
+
+	fmt.Println(" map is : ",m)
+
+	m["k1"] = 1
+	m["k2"] = 2
+
+	fmt.Println(" map is : ",m)
+	fmt.Println(" length of map ",len(m))
+
+	v := m["k1"]
+	fmt.Println(" value of k1 is",v)
+
+	delete(m,"k2")
+	fmt.Println(" map is : ",m)
+
+	n := map[string]int{"abc" : 1,"xyz" : 2}
+	fmt.Println(" map is : ",n)
+
+	
+
+
+
+}
