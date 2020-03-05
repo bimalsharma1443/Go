14f6118c239d632cf4886a0b935305b19e4ef8bc
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-03-05 21:38:51 +0530

    clouser change

---
+++ src/gobyexample/Clouser.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	func () {
+		fmt.Println("Clouser function")
+	}()
+
+
+	incInt := call()
+
+	fmt.Println(incInt())
+	fmt.Println(incInt())
+	fmt.Println(incInt())
+
+	incInt1 := call()
+
+	fmt.Println(incInt1())
+	fmt.Println(incInt1())
+	fmt.Println(incInt1())
+}
+
+func call() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
---
+++ src/gobyexample/MultiReturn.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func sumAndMulti(value ...int) (int,int){
+	sum := 0
+	multi := 1
+
+	for _, v := range value {
+		sum+=v
+		multi*=v
+	}
+
+	return sum,multi
+
+}
+
+
+func main() {
+
+	sum,multi := sumAndMulti(1,2,3,4)
+
+	fmt.Printf("Sum is %v and Multi is %v",sum,multi)
+	
+
+
+}
