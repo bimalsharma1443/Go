2d52122c1e4ddbb4e7bc92818082ba5f9c1f7710
Bimal Sharma <Administrator@SSLBCGWC0R16155.SBICAPSECU.COM>
2020-03-05 23:29:55 +0530

    recursion

---
+++ src/gobyexample/Recursion.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i uint64
+
+	fmt.Println(" Please enter a number ")
+	
+	fmt.Scanln(&i)
+
+	fmt.Printf("factorial of %v is %v",i,fact(i))
+}
+
+func fact(f uint64) uint64 {
+
+	if f == 1 {
+		return 1
+	}else {
+		return f*fact(f-1)
+	}
+}
